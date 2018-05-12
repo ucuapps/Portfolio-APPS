9a50d412878c0d3932c84be5c6fc714bcc2cb02b
Inkognita <liepieshov@ucu.edu.ua>
2018-05-12 17:21:59 +0300

    Adding First/Last name to signup

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from django.forms import forms
+from django import forms as f
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.socialaccount.helpers import ImmediateHttpResponse
@@ -20,3 +21,15 @@ class SocialDomainCheckAdapter(DefaultSocialAccountAdapter):
         if not u.email.split('@')[1] == "ucu.edu.ua":
             raise ImmediateHttpResponse(HttpResponse("It is not allowed"))
         return super().pre_social_login(request, sociallogin)
+
+
+class CustomSignupForm(f.Form):
+    first_name = f.CharField(max_length=30, label='First name',
+                             widget=f.TextInput(attrs={'placeholder': 'Your first name'}))
+    last_name = f.CharField(max_length=30, label='Last name',
+                            widget=f.TextInput(attrs={'placeholder': 'Your last name'}))
+
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
