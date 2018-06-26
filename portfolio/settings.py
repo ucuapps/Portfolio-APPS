6c142045ed16b1998ba22b9ba556d0c1825683ce
irynakostyshyn <i.kostyshyn@ucu.edu.ua>
2018-06-26 13:22:40 +0300

    student info example added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     }
 }
 
-# Password validation
+# Password validationzz
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
