387a2dadbdec39bed8c3db6704ba3faae49a6ce7
server <server@ucu.edu.ua>
2018-07-10 14:27:33 +0000

    production

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
+os.environ["EMAIL_HOST_USER"] = "help.portfolio.apps@gmail.com"
+os.environ["EMAIL_HOST_PASSWORD"] = "hgq@e13n4h"
+os.environ["DATABASE_PASSWORD"] = "theLupA17kKLRPopt13k"
+os.environ["DATABASE_USERNAME"] = "portfoliouser"
+
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+
 
 application = get_wsgi_application()
