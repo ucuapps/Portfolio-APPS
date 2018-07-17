d3f552d763f712a41ab7b8854428ee4ef9168287
famdrum <o.kozak@ucu.edu.ua>
2018-07-17 13:23:20 +0300

    changed student info

--- static/static/js/menu_mover.js
+++ static/static/js/menu_mover.js
@@ -11,13 +11,23 @@ function closeNav() {
 }
 
 function popup_func() {
-    var popup = document.getElementById("popup");
+    var popup = document.getElementsByClassName("popup")[0];
+    var popup_request = document.getElementsByClassName("popup")[1];
     popup.style.display = "flex";
+    popup_request.style.display = "none";
+}
+function popup_request_func() {
+    var popup = document.getElementsByClassName("popup")[0];
+    var popup_request = document.getElementsByClassName("popup")[1];
+    popup.style.display = "none";
+    popup_request.style.display = "flex";
 }
 
 document.onclick = function(e){
     var popup = document.getElementById("popup");
-    if(e.target.className !== "avatar" && e.target.id !== "popup"){
-        popup.style.display = 'none';
+    for(var i = 0; i < popup.length; i++){
+        if(e.target.className !== "avatar" && e.target.id !== "popup"){
+            popup.style.display = 'none';
+        }
     }
 };
