ff03b72c06f2595144f587bf3f888a104074c794
Yurii Stasinchuk <yra100@gmail.com>
2018-10-06 16:45:44 +0200

    Update config.py

--- ccalendar/config.py
+++ ccalendar/config.py
@@ -2,7 +2,7 @@ import os
 
 google_path = ""
 
-if os.environ['ADM_JSON'] is None:
+if os.environ['ADM_JSON'] is not None:
         google_path = os.environ['ADM_JSON']
 else:
     google_path = '/Users/StasMaster/Downloads/adm.json'
