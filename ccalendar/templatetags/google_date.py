c10fd406559ef68fae5870bf1631b0fb4a7e82bf
Yrii Stasinchuk <stasinchuk@ucu.edu.ua>
2019-01-24 16:17:33 +0100

    add_space

--- ccalendar/templatetags/google_date.py
+++ ccalendar/templatetags/google_date.py
@@ -72,7 +72,7 @@ def to_minute(value):
 @register.filter(name='location')
 def location(value):
     value= value.split(" ")[0]
-    return value
+    return value + "   "
 
 
 @register.filter(name='color_filter')
